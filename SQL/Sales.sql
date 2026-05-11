d5b25d7f92d0af8ea61f071c2b5f1c3801ffff00
jajfasfaa <manuelabreu1212@protonmail.com>
2026-05-11 13:35:09 -0500

    table

--- SQL/Sales.sql
+++ SQL/Sales.sql
@@ -1,3 +1,37 @@
-select COUNT(*)
-from DimAccount;
-
+DROP VIEW IF EXISTS currency;
+GO
+CREATE VIEW currency as
+    SELECT top 15 * 
+    from DimCurrency;
+GO
+DROP view IF EXISTS vwDimAccount;
+GO
+Create view vwDimAccount AS
+    select Accountkey,accounttype,AccountDescription,Operator,Valuetype
+    from DimAccount;
+GO
+drop view IF EXISTS vwFactFinance;
+GO
+create VIEW vwFactFinance AS 
+    SELECT financekey,organizationkey,DepartmentGroupKey,ScenarioKey,AccountKey,Amount
+    from FactFinance;
+GO
+drop view IF EXISTS vwDimCustomer;
+GO
+create VIEW vwDimCustomer AS
+    SELECT CustomerKey,geographykey,concat(firstname, ' ', middlename, ' ', lastname) as CustomerName,
+        birthdate,yearlyincome
+    from DimCustomer;
+GO
+drop view IF EXISTS vwDimGeography;
+GO
+create VIEW vwDimGeography AS 
+    SELECT GeographyKey,City,StateProvinceName,EnglishCountryRegionName As region,SalesTerritoryKey
+    from DimGeography;
+GO
+DROP view if exists vwDimProduct
+GO 
+CREATE VIEW vwDimProduct AS
+    SELECT ProductKey,EnglishProductName as ProductName,ProductSubcategoryKey,SafetyStockLevel,ReorderPoint,[Status]
+    from DimProduct
+    where status = 'Current';
