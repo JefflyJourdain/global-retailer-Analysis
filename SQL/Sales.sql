f8d7b4bdf7513b619d9ee324d71d6a873a70adb3
jajfasfaa <manuelabreu1212@protonmail.com>
2026-05-11 19:17:42 -0500

    more tables

--- SQL/Sales.sql
+++ SQL/Sales.sql
@@ -35,3 +35,30 @@ CREATE VIEW vwDimProduct AS
     SELECT ProductKey,EnglishProductName as ProductName,ProductSubcategoryKey,SafetyStockLevel,ReorderPoint,[Status]
     from DimProduct
     where status = 'Current';
+GO
+drop view IF EXISTS vwfactinternetsales;
+GO
+create VIEW vwfactinternetsales AS
+        
+    SELECT ProductKey,CustomerKey,SalesOrderNumber,TotalProductCost,OrderQuantity,UnitPrice,ExtendedAmount,taxamt as TaxAmount,Freight,
+    SalesTerritoryKey,OrderDate,DueDate,ShipDate
+    from factinternetsales;
+GO
+drop view if EXISTS vwdimreseller;
+GO
+create VIEW vwdimreseller AS
+    SELECT ResellerKey,GeographyKey,ResellerName,AnnualSales,AnnualRevenue,YearOpened
+    from DimReseller;
+GO
+drop view if exists vwfactproductinventory;
+GO
+create VIEW vwfactproductinventory AS
+    SELECT ProductKey,MovementDate,UnitCost,UnitsIn,UnitsOut,UnitsBalance
+    from factproductinventory;
+go
+drop view if exists vwfactresellersales;
+GO
+create VIEW vwfactresellersales AS
+    SELECT ProductKey,ResellerKey,SalesOrderNumber,TotalProductCost,OrderQuantity,UnitPrice,ExtendedAmount,taxamt as TaxAmount,Freight,
+    SalesTerritoryKey,OrderDate,DueDate,ShipDate
+    from factresellersales;
